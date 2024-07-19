6520a308fdc5b704f8a302a0b4e55374e5944362
rngrhn4114 <130418820+rngrhn4114@users.noreply.github.com>
2024-07-19 20:40:02 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,12 +1,2 @@
 def solution(binomial):
-    answer = 0
-    binomial_divi = binomial.split(' ')
-    if binomial_divi[1] == '+':
-        answer = int(binomial_divi[0]) + int(binomial_divi[2])
-    elif binomial_divi[1] == '-':
-        answer = int(binomial_divi[0]) - int(binomial_divi[2])
-    elif binomial_divi[1] == '*':
-        answer = int(binomial_divi[0]) * int(binomial_divi[2])
-    elif binomial_divi[1] == '/':
-        answer = int(binomial_divi[0]) / int(binomial_divi[2])
-    return answer
+    return eval(binomial)
